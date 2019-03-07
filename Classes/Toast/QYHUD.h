91250ba0a1956f555762c045cc14e18159f47b76
InsectQY <704861917@qq.com>
2019-03-07 13:48:38 +0800

    1

---
+++ Classes/Toast/QYHUD.h
@@ -0,0 +1,23 @@
+//
+//  QYHUD.h
+//  Mine_Example
+//
+//  Created by apple on 2019/3/6.
+//  Copyright © 2019 704861917@qq.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QYHUD : NSObject
+
++ (void)showLoadingInView:(UIView *)view;
+
++ (void)showLoading:(NSString *)text inView:(UIView *)view;
+
++ (void)showSucceed:(nullable NSString *)text;
+
++ (void)showError:(nullable NSString *)text;
+
++ (void)showInfo:(nullable NSString *)text;
+
+@end
---
+++ Classes/Toast/QYHUD.m
@@ -0,0 +1,36 @@
+//
+//  QYHUD.m
+//  Mine_Example
+//
+//  Created by apple on 2019/3/6.
+//  Copyright © 2019 704861917@qq.com. All rights reserved.
+//
+
+#import "QYHUD.h"
+#import <QMUIKit/QMUITips.h>
+
+@implementation QYHUD
+
++ (void)showLoadingInView:(UIView *)view {
+    [QMUITips showLoadingInView:view];
+}
+
++ (void)showLoading:(NSString *)text inView:(UIView *)view {
+    [QMUITips showLoading:text inView:view];
+}
+
++ (void)showSucceed:(nullable NSString *)text {
+    [QMUITips showSucceed:text];
+}
+
++ (void)showError:(nullable NSString *)text {
+    [QMUITips showError:text];
+}
+
++ (void)showInfo:(nullable NSString *)text {
+    [QMUITips showInfo:text];
+}
+
+
+
+@end
